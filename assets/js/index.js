4974a0b8ae94d33c8251e53fda56c9dbd1a375d7
애브리띵 <66000432+OHvrything@users.noreply.github.com>
2020-12-17 16:04:08 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -93,38 +93,3 @@ Kakao.Story.createShareButton({
 })
 
 Kakao.init('88a0911c2ba4f1a6b0392f4b3dd53221');
-
-function shareKatalk() {
-Kakao.Link.sendScrap({
-    requestUrl: location.href
-});
-};
-
-function CopyUrlToClipboard()
-
-{
-
-	var obShareUrl = document.getElementById("ShareUrl");
-
-	obShareUrl.value = window.document.location.href;  // 현재 URL 을 세팅해 줍니다.
-
-
-
-	obShareUrl.select();  // 해당 값이 선택되도록 select() 합니다
-
-	document.execCommand("copy"); // 클립보드에 복사합니다.
-
-
-
-	obShareUrl.blur(); // 선택된 것을 다시 선택안된것으로 바꿈니다.
-
-		
-
-	alert("URL이 클립보드에 복사되었습니다"); 
-
-}
-
-function Loadingcomplete()
-var obShareUrl = document.getElementById("ShareUrl");
-
-obShareUrl.value = window.document.location.href;
